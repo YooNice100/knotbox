07162bdb4ec05e1d65b9008eab54b8137f613c04
Eunice <eunicecho100@gmail.com>
2026-05-08 22:28:27 -0700

    patternLink.js setting it up

---
+++ src/data/patternLinks.js
@@ -0,0 +1,20 @@
+export const patternLinks = [
+  {
+    id: 1,
+    category: "Farm",
+    name: "White Chicken",
+    url: "https://example.com",
+  },
+  {
+    id: 2,
+    category: "Water",
+    name: "Big Turtle",
+    url: "https://example.com",
+  },
+  {
+    id: 3,
+    category: "Random",
+    name: "Cookie",
+    url: "https://example.com",
+  },
+];
